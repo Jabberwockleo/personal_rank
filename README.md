765b2651a6ae699ccd93876914dbed6590f935ce
Leo Wan <jabberwockwanli@gmail.com>
2018-05-13 15:36:58 +0800

    pr

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # personal_rank
+
 Graph-based recommendation toy codes
+
 References:
+
 [Fouss, P., et al., 2006]
+
 [Haveliwala, 2002]
--- src/personal_rank_bipartite.py
+++ src/personal_rank_bipartite.py
@@ -52,18 +52,34 @@ def candidate_nodes(G, node):
     return set(r).difference(set(G.adj[node]))
 
 
+def load_user_items(fn):
+    """
+        User & Items
+    """
+    users = set()
+    items = set()
+    for line in open(fn, 'r'):
+        elems = line.rstrip().split(',')
+        if len(elems) > 0:
+            users.add(elems[0])
+        if len(elems) > 1:
+            items |= set(elems[1:])
+    return users, items
+
+
 def recommend(fn, export_graph=True):
     """
         Recommendation
     """
     fd = open('out_pr_recom.csv', 'w')
-    G = load_graph(fn)
-    layout =  bipartite_layout(G)
-    users, items = nx.bipartite.sets(G)
-    pr = nx.pagerank(G, alpha=0.85, \
-        weight=None, personalization=None, max_iter=100)
+    G_raw = load_graph(fn)
+    users, items = load_user_items(fn)
     recom_dict = {}
     for user in users:
+        nodes = nx.node_connected_component(G_raw, user)
+        G = G_raw.subgraph(nodes)
+        pr = nx.pagerank(G, alpha=0.85, \
+            weight=None, personalization=None, max_iter=100)
         recoms = sorted([(n, round(pr[n], 3)) for n in candidate_nodes(G, user)], \
             key=lambda x:x[1], reverse=True)
         fd.write('{},'.format(user))
@@ -74,11 +90,20 @@ def recommend(fn, export_graph=True):
     if export_graph is True:
         plt.figure()
         plt.title(fn)
-        nx.draw(G, pos=layout, \
-            node_size=[x * 1e4 for x in pr.values()], with_labels=True)
+        pr = nx.pagerank(G, alpha=0.85, \
+            weight=None, personalization=None, max_iter=100)
+        nc = []
+        for n in G.nodes:
+            if n in users:
+                nc.append('r')
+            else:
+                nc.append('b')
+        nx.draw_networkx(G, pos=nx.spring_layout(G), \
+            node_size=[x * 1e4 for x in pr.values()], node_color=nc, \
+            with_labels=True)
         plt.savefig('out_pagerank_figure.jpg')
     return recom_dict
 
 if __name__ == "__main__":
-    recommend("../data/simple_user_item.csv", export_graph=False)
+    recommend("../data/simple_user_item.csv", export_graph=True)
 
